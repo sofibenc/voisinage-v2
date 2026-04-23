3d51e827247d8c59bd451b32685bb8a994bf1f06
Sofiane <sofiane.benchouk-ext@mfi.fr>
2026-04-23 14:46:22 +0200

    feat: rechargement automatique lors de la mise à jour du service worker

--- src/main.jsx
+++ src/main.jsx
@@ -6,3 +6,9 @@ import App from './App.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.location.reload();
+  });
+}
